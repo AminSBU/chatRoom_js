5157472072796e6acc24cdad99977cc45eda7f8b
Amin Bakhshoodeh <bakhshoodeh.amin@mapnaec.com>
2026-06-06 19:04:22 +0330

    consider input text and in right position

--- chatroom/src/Chatbox.jsx
+++ chatroom/src/Chatbox.jsx
@@ -6,10 +6,22 @@ function Chatbox()
     return(
         <>
             <div
-            className="w-full h-full bg-cover bg-center bg-no-repeat"
-                style={{ backgroundImage: `url(${heroImg})` }}
-                >
-                <p1>Chatbox</p1>
+            className="w-full h-full bg-cover bg-center bg-no-repeat flex flex-col"
+            style={{ backgroundImage: `url(${heroImg})` }}
+            >
+            {/* فضای پیام‌ها */}
+            <div className="flex-1 p-4 text-white">
+                <h1>Chatbox</h1>
+            </div>
+
+            {/* input پایین صفحه */}
+            <div className="p-4">
+                <input
+                type="text"
+                placeholder="پیام خود را بنویس..."
+                className="w-full p-3 rounded-lg border"
+                />
+            </div>
             </div>
         </>
     )
